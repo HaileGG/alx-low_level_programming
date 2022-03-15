32ee8f8b399fc664affaa38e3c75ce31a2f8742d
Haile Gebresilassie <hailye2013@gmail.com>
2022-03-15 12:19:45 -0700

    completed task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 
 /**
  * print_sign - Determines whether an integer is positive negative or zero
- * @c: the number to be checked
+ *
  * Return: 1 if positive, 0 if zero, -1 if negative, /if not a digit
  */
 int print_sign(int n)
@@ -11,20 +11,20 @@ int print_sign(int n)
 	{
 		_putchar('+');
 		return (1);
-        }
+	}
 	else if (n == 0)
 	{
 		_putchar('0');
 		return (0);
-        }
+	}
 	else if (n < 0)
-	{	
+	{
 		_putchar('-');
 		return (-1);
-        }
+	}
 	else
 	{
 		_putchar(' ');
-		return ('/');
-        }
+		return ('/')
+	}
 }
