bd89da2815868363477c3d8a3c7ce49f11ad94d1
Haile Gebresilassie <hailye2013@gmail.com>
2022-03-15 13:22:23 -0700

    Completed task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,26 +7,26 @@
  */
 void times_table(void)
 {
-	int rone, cone, d;
+	int i, j, k;
 
-	for (rone = 0; rone <= 9; rone++)
+	for (i = 0; i <= 9; i++)
 	{
 		_putchar('0');
 		_putchar(',');
 		_putchar(' ');
-		for (cone = 1; cone <= 9; cone++)
+		for (j = 1; j <= 9; j++)
 		{
-			if ((d / 10) > 0)
+			if ((k / 10) > 0)
 			{
-				_putchar((d / 10) + '0');
+				_putchar((k / 10) + '0');
 			}
 			else 
 			{
 				_putchar(' ');
 			}
-			_putchar((d % 10) + '0');
+			_putchar((k % 10) + '0');
 
-			if (cone < 9)
+			if (j < 9)
 			{
 				_putchar(',');
 				_putchar(' ');
