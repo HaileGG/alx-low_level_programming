a1baaf1f548ee5141f0d248bfc98d725fbc44d6e
Haile Gebresilassie <hailye2013@gmail.com>
2022-03-15 03:31:59 -0700

    Completed task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,13 @@
-#include <main.h>
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+int _putchar( char c)
 {
-	_putchar("Hello C");
-	_putchar('\n');
 
-	return (0);
+	return (write(1, &c, 1));
 }
