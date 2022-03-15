83dca98c54c51da443b0c9481fc1fe6cf6f4d964
Haile Gebresilassie <hailye2013@gmail.com>
2022-03-15 14:32:21 -0700

    Completed task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,33 +8,26 @@
  */
 void print_to_98(int n)
 {
-	if (n < 98)
+	int i, j;
+
+	if (n <= 98)
 	{
-		while (n <= 98)
+		for (i = n; i <= 98; i++)
 		{
-			printf("%d", n);
-			if (n != 98)
-			{
-				printf(", ");
-			}
-			n++;
+			if (i != 98)
+				printf("%d", i);
+			else if(i == 98)
+				printf("%d\n", i);
 		}
 	}
-	else if (n > 98)
+	else if(n >= 98)
 	{
-		while (n >= 98)
+		for (j = n; j >= 98; j++)
 		{
-			printf("%d", n);
-			if (n != 98)
-			{
-				printf(", ");
-			}
-			n++;
+			if (j != 98)
+				printf("%d", j);
+			else if (j == 98)
+				printf("%d\n", j);
 		}
 	}
-	else 
-	{
-		printf("98");
-	}
-	printf("\n");
 }
