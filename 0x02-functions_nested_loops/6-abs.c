01e5633f01f4d67baa1a3158a2fa5f6958ec5e1f
Haile Gebresilassie <hailye2013@gmail.com>
2022-03-15 12:45:34 -0700

    Completed task 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,7 +2,7 @@
 
 /**
  * _abs - calculate the absolute value os the number from zero.
- * @c: the integer to be checked.
+ * @i: the integer to be checked.
  * Return: absolute value of the input number.
  */
 int _abs(int i)
