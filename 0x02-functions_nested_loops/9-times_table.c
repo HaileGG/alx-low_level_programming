a5dfe696538eacefd7ea91854d9ff76bc8d97aad
Haile Gebresilassie <hailye2013@gmail.com>
2022-03-15 15:04:46 -0700

    Completed task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,9 +11,9 @@ void times_table(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j <10; j++)
+		for (j = 0; j < 10; j++)
 		{
-			k = j*i;
+			k = j * i;
 			if (j == 0)
 			{
 				_putchar(k + '0');
@@ -21,6 +21,7 @@ void times_table(void)
 			if (k < 10 && j != 0)
 			{
 				_putchar(',');
+				_putchar(' ');
 				_putchar(' ');
 				_putchar(k + '0');
 			}
