031ee38e8fc6260bd8f7fc1d9e67ffd6c51ec72c
Haile Gebresilassie <hailye2013@gmail.com>
2022-03-15 14:05:35 -0700

    Completed task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * print_to_98 - a function that prints all natural numbers from n to 98
+ * @n: int natural number
+ * Return: Always 0 (Success)
+ */
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n <= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n++;
+		}
+	}
+	else if (n > 98)
+	{
+		while (n >= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				ptintf(", ");
+			}
+			n++;
+		}
+	}
+	else 
+	{
+		ptintf("98");
+	}
+	printf("\n");
+}
