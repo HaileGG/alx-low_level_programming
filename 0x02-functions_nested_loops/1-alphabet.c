ff2d6e324b1cb6ccbbf19d8f8ba31ad2196e424a
Haile Gebresilassie <hailye2013@gmail.com>
2022-03-15 11:26:49 -0700

    completed task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - a function taht prints the aplphabet, in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+
+	_putchar('\n');
+}
