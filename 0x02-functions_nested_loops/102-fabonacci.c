20c1840a0a5ffd207be69e1650fea30f6d5ccd73
Haile Gebresilassie <hailye2013@gmail.com>
2022-03-15 22:45:21 -0700

    Completed task 14

---
+++ 0x02-functions_nested_loops/102-fabonacci.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print the first 50 fabonacci numbers.
+ * Return: Nothing.
+ */
+int main(void)
+{
+	unsigned long c, i, j, k;
+
+	i = 0;
+	j = 1;
+	for (c = 0; c <= 50; c++)
+	{
+		k = i + j;
+		i = j;
+		j = k;
+		printf("%d", k);
+		if (c == 49)
+			putchar('\n');
+		else
+		{
+			printf(", ");
+		}
+	}
+	return (0);
+
+
+}
