f9be1db11981e63482dc41da97688d2add0f8577
Haile Gebresilassie <hailye2013@gmail.com>
2022-03-15 22:35:03 -0700

    Completed task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - computes and prints the sum off all the multiple of
+ * 3 or 5 below 1024.
+ * Return: Nothing.
+ */
+int main(void)
+{
+	int i, r;
+
+	for (i = 0; i < 1024; i++)
+		if (i % 3 == 0 || i % 5 == 0)
+			r += i;
+
+	printf("%d\n", r);
+	return ()0;
+}
