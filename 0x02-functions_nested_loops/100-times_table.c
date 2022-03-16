d5de5ef95c45e349cf5a2642b1046c826eb6c5a5
Haile Gebresilassie <hailye2013@gmail.com>
2022-03-15 22:28:50 -0700

    Completed task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -36,7 +36,7 @@ void print_times_table(int n)
 				else if (r >= 100 && j != 0)
 				{
 					_putchar((r / 100) + '0');
-					_putchar((r / 10) + '0');
+					_putchar((r / 10) % 10  + '0');
 					_putchar((r % 10) + '0');
 				}
 				else
