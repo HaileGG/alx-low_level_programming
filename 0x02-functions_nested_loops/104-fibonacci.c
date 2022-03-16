f7a37c4355e70c7dc47760f5ef60f311a9ff9e5c
Haile Gebresilassie <hailye2013@gmail.com>
2022-03-15 23:18:47 -0700

    Completed task 16

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+/**
+ * main print the first 98 fibonacci numbers.
+ * Return: Nothing.
+ */
+int main(void)
+{
+	int c;
+	unsigned long i, j, k;
+	unsigned long m, n, p, cr;
+
+	c = 0;
+	i = 0;
+	j = 1;
+	for (c = 1; c <= 91; c++)
+	{
+		k = i + j;
+		i = j;
+		j = k;
+		printf("%lu. ", k);
+	}
+	m = i % 1000;
+	i = i / 1000;
+	n = j % 1000;
+	j = j / 1000;
+	while (c <= 98)
+	{
+		cr = (m + n) / 1000;
+		p = (m + n) - cr * 1000;
+		k = (i + j) + cr;
+		m = n;
+		n = p;
+		i = j;
+		j = k;
+		if (p >= 100)
+			prntf("%lu%lu", k, p);
+		else 
+			printf("%lu%lu", k, p);
+		if (c != 98)
+			printf(", ");
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
