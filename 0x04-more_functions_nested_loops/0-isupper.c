3aaed013d253398ad81d2fa10c5e8ad3ce24a920
Haile Gebresilassie <hailye2013@gmail.com>
2022-03-17 11:04:44 -0700

    Completed task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,13 +3,13 @@
 /**
  * _isupper - checks for uppercase character.
  *
- * @c: Variable
+ * @c: The character to checked.
  *
  * Retrun: 1 if c is upercase 0 otherwise.
  */
 int _isupper(int c)
 {
-	if (c >= 'A' && (c <= 'Z'))
+	if (c >= 'A' && c <= 'Z')
 	{
 		return (1);
 	}
