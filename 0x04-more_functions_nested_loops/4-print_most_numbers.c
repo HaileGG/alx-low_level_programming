1b3d4dcb9e662c44d8cc951822fca52646f598a7
Haile Gebresilassie <hailye2013@gmail.com>
2022-03-17 03:43:08 -0700

    Completed task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,15 +9,12 @@ void print_most_numbers(void)
 {
 	int a;
 
-	for (a = 48; a < 58; a++)
+	for (a = 0; a <= 9; a++)
 	{
-		if (a != 50)
-		{
-			if (a != 52)
-			{
-				_putchar(a);
-			}
-		}
+		if ((a == 2 || (a == 4))
+				continue;
+		else
+			_putchar(a + '0');
 	}
 	_putchar('\n');
 }
