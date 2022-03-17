d212f3a5816553bf064f5c6830a168cb61623344
Haile Gebresilassie <hailye2013@gmail.com>
2022-03-17 06:53:59 -0700

    Completed task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,16 +2,18 @@
 
 /**
  * print_numbers - prints the numbers from 0 through 9
+ *
  * @a: Variable
- * Return: void
+ *
+ * Return: Always 0
  */
 void print_numbers(void)
 {
 	int a;
 
-	for (a = '0'; a <= '9'; a++)
+	for (a = 0; a <= 9; a++)
 	{
-		_putchar(a);
+		_putchar('0' + a);
 	}
 	_putchar('\n');
 }
