4d80610a71709a271bd090fa43905dbeedd59a4e
Haile Gebresilassie <hailye2013@gmail.com>
2022-03-17 11:45:19 -0700

    Completed task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,19 +2,22 @@
 
 /**
  * print_most_numbers - prints most numbers from 0 through 9
- * @a: Variable
+ * 
  * Return: Always 0
  */
 void print_most_numbers(void)
 {
 	int a;
 
-	for (a = '0'; a <= '9'; a++)
+	for (a = 48; a < 58; a++)
 	{
-		if ((a == '2') || (a == '4'))
-			;
-		else
-			_putchar(a);
+		if (a != 50)
+		{
+			if (a != 52)
+			{
+				_putchar(a);
+			}
+		}
 	}
 	_putchar('\n');
 }
