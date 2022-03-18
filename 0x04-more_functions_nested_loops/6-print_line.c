58696b3c940c4972e29101a4d3652a3b0fcec14b
Haile Gebresilassie <hailye2013@gmail.com>
2022-03-17 22:40:02 -0700

    Completed task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,20 +1,17 @@
 #include "main.h"
 
 /**
- * print_line - prints the straight lines
- *
- * @x: Variable
- *
+ * print_line - prints the straight lines.
+ * @n: n - Variable.
  * Return: Always 0
  */
-
 void print_line(int n)
 {
 	int x = 0;
 
 	if (n > 0)
 	{
-		for (x = 1; x < n; x++)
+		for (x = 1; x <= n; x++)
 		{
 			_putchar('_');
 		}
