c346d863aa7609441ff8ce028e3eacf980e6c341
Haile Gebresilassie <hailye2013@gmail.com>
2022-03-17 22:01:06 -0700

    Completed task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -3,34 +3,31 @@
 /**
  * print_triangle - prints the triangle line
  *
- * @size: Variable.
+ * @size: Size of triangle.
  *
  * Return: Always 0
  */
 void print_triangle(int size)
 {
-	int x, y;
+	int s, r, t;
 
-	int (size > 0)
+	if (size <= 0)
 	{
-		for (x = 1; x <= size; x++)
-		{
-			for (y = 1; y <= size; y++)
-			{
-				if (y <= size - x)
-				{
-					_putchar(' ');
-				}	
-				else
-				{
-					_putchar('#');
-				}
-			}
-			_putchar('\n');
-		}
+		_putchar('\n');
 	}
 	else
 	{
-		_putchar('\n');
+		for (r = 0; r <= (size - 1); r++)
+		{
+			for (s = 0; s <= (size - 1) - r; s++)
+			{
+				_putchar(' ');
+			}
+			for (t = 0; t <= r; t++)
+			{
+				_putchar('#');
+			}
+			_purchar('\n');
+		}
 	}
 }
