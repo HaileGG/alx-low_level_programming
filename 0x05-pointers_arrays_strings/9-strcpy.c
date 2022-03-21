5e3859935b3a073352c6796d47a5ffd53d090bc3
Haile Gebresilassie <hailye2013@gmail.com>
2022-03-21 00:59:11 -0700

    Completed task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * char *_strcpy - function that copies the string pointed to by str.
+ * @dest: destination of the string.
+ * @src: source of the string.
+ * Return: string.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *guard = dest;
+
+	while (*str)
+	{
+		*dest++ = *str++;
+		*dest = 0;
+	}
+	return (guard);
+}
