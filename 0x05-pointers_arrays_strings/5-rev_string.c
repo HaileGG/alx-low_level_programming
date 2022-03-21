9fc094151fa9196ceb6b4089deeadcadb2adc4c7
Haile Gebresilassie <hailye2013@gmail.com>
2022-03-21 00:40:26 -0700

    Completed task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - function that prints the string reverse.
+ * @s: input to reverse.
+ * Return: string revers.
+ */
+void rev_string(char *s)
+{
+	char r = s[0];
+	int f = 0;
+	int i;
+
+	while (s[f] != '\0')
+		f++;
+
+	for (i = 0; i < f; i++)
+	{
+		f++;
+		r = s[i];
+		s[i] = s[f];
+		s[f] = r;
+	}
+}
