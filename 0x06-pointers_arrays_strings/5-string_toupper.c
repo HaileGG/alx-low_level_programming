51516cced304b280dfc7e12b1115a5b3d3ae0e6a
Haile Gebresilassie <hailye2013@gmail.com>
2022-03-22 23:21:37 -0700

    Copmleted task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - change lowercase to uppercase.
+ * @s: string.
+ * Return: char
+ */
+char _string_toupper(char *s)
+{
+	int i = 0;
+
+	while (*(s + i))
+	{
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+		{
+			*(s + i) -= '' - 'A';
+			i++;
+		}
+	}
+	return (0);
+}
