384cab0429a607bf5b4d55ff8f14b503df31d49b
Haile Gebresilassie <hailye2013@gmail.com>
2022-03-24 00:40:07 -0700

    Completed task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,21 +10,22 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
 
-	i = 0;
-	while (dest[i] != '\0')
+	i = j = 0;
+	while (*(dest + i))
 	{
 		i++;
 	}
 
-	j = 0;
-	while (j < n && src[j] != '\0')
+	while (j < n && *(src + j))
 	{
-		dest[i] = src[j];
+		*(dest + i) = *(src + j);
+		i++;
 		j++;
-		i++;
 	}
 
-	dest[i] = '\0';
-
+	if (j < n)
+	{
+		*(dest + i) = *(src + j);
+	}
 	return (dest);
 }
