26192ee3fe2f22c726e0f48231e57a43b8c9eeb6
Haile Gebresilassie <hailye2013@gmail.com>
2022-03-24 22:35:50 +0300

    Completed task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,27 +1,24 @@
 #include "main.h"
 
 /**
- * *leet - encodes a string into 1337.
- * @s: string.
- * Retrun: adress of s.
+ * leet - encodes a string into 1337
+ * @s: string to encode
+ *
+ * Return: address of s
  */
 char *leet(char *s)
 {
 	int i, j;
+	char a[] = "aAeEoOtTlL";
+	char b[] = "4433007711";
 
-	char *a = "aAeEoOtTlL";
-	char *b = "4433007711";
-	
-	for (i = 0; s[i] != '\0'; i++) 
+	for (i = 0; *(s + i); i++)
 	{
-		for (j = 0; s[j] < 10; j++)
+		for (j = 0; j <= 9; j++)
 		{
-			if (s[i] == a[j])
-			{
+			if (a[j] == s[i])
 				s[i] = b[j];
-			}
 		}
 	}
-
 	return (s);
 }
