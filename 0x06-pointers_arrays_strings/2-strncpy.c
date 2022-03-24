578e63b4e9913c0dafb65a6ca4df2bab5a4f006b
Haile Gebresilassie <hailye2013@gmail.com>
2022-03-23 22:48:26 -0700

    Completed task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,23 +1,28 @@
 #include "main.h"
 
 /**
- * _strncpy - copie a string.
- *
- *
+ * _strncpy - copies a string.
+ * @dest: destination value.
+ * @src: source value.
+ * @n: final value.
+ * Return: dest.
  */
 char _strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
+	int i;
 
-	ahile (i < n && *(src + i))
+	i = 0;
+	while (i < n && src[i] != '\0')
 	{
-		*(dest + i) = *(src + i);
+		dest[i] = src[i];
 		i++;
 	}
+
 	while (i < n)
 	{
-		*(dest + i) = '\0';
+		dest[i] = '\0';
 		i++;
 	}
+
 	return (dest);
 }
