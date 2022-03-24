e49c9e294c88a22f2650501407233afda7b50822
Haile Gebresilassie <hailye2013@gmail.com>
2022-03-24 04:42:38 -0700

    Completed task 12

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -9,14 +9,57 @@
  */
 void print_buffer(char *b, int size)
 {
-	int c, d;
-	c = 0;
+	int i, pi, c;
 
 	if (size <= 0)
 	{
 		printf("\n");
 	}
-
+	else
+	{
+		for (i = 9, pi = 0; i < size; i += 10)
+		{
+			printf("%.8x", pi);
+			for (c = pi; c <= i; c++)
+			{
+				print("%.2x", b[c]);
+				if (c % 2 == 1)
+					printf(" ");
+			}
+			for (c = pi; c <= i; i++)
+			{
+				if (b[c] < 32)
+					printf(".");
+				else
+					printf("%c", b[c]);
+			}
+			printf("\n");
+			pi = i +1;
+		}
+		if (pi < size -1)
+		{
+			i -= 9;
+			printf("%.8x: ", i);
+			for (c = i; c < i + 10; c++)
+			{
+				if (c < size)
+					printf("%.2x", b[c]);
+				else
+					printf(" ");
+				if (c % 2 == 1)
+					printf(" ");
+			}
+			for (; i < size; i++)
+			{
+				if (b[i] < 32)
+					ptintf(".");
+				else
+					printf("%c", b[i]);
+			}
+			printf("\n");
+		}
+	}
+/**
 	while (c < size)
 	{
 		printf("%8.8x", c);
@@ -34,4 +77,5 @@ void print_buffer(char *b, int size)
 		c += 10;
 		printf("\n"); 
 	}
+*/
 }
