85ecb37f0c96e4eeaf41f7775a63933206d2353f
Haile Gebresilassie <hailye2013@gmail.com>
2022-03-24 09:34:27 -0700

    Completed task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,15 +1,15 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
  * _strncat - concatenates two strings.
  * @dest: destination value.
  * @src: source value.
  * @n: limit of the concatenation.
+ * Return: dest
  */
 char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
-
 	i = 0;
 
 	while (dest[i] != '\0')
@@ -18,7 +18,6 @@ char *_strncat(char *dest, char *src, int n)
 	}
 
 	j = 0;
-
 	while (j < n && src[j] != '\0')
 	{
 		dest[i] = src[j];
@@ -26,6 +25,5 @@ char *_strncat(char *dest, char *src, int n)
 		j++;
 	}
 	dest[i] = '\0';
-
 	return (dest);
 }
