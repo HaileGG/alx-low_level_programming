aed140aa0a3ddc8092cea5a50c56800375c2e6e9
Haile Gebresilassie <hailye2013@gmail.com>
2022-03-24 04:46:38 -0700

    Completed task 12

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -9,65 +9,16 @@
  */
 void print_buffer(char *b, int size)
 {
-	int i, pi, c;
+	int c, d;
 
-	if (size <= 0)
-	{
-		printf("\n");
-	}
-	else
-	{
-		for (i = 9, pi = 0; i < size; i += 10)
-		{
-			printf("%.8x", pi);
-			for (c = pi; c <= i; c++)
-			{
-				print("%.2x", b[c]);
-				if (c % 2 == 1)
-					printf(" ");
-			}
-			for (c = pi; c <= i; i++)
-			{
-				if (b[c] < 32)
-					printf(".");
-				else
-					printf("%c", b[c]);
-			}
-			printf("\n");
-			pi = i +1;
-		}
-		if (pi < size -1)
-		{
-			i -= 9;
-			printf("%.8x: ", i);
-			for (c = i; c < i + 10; c++)
-			{
-				if (c < size)
-					printf("%.2x", b[c]);
-				else
-					printf(" ");
-				if (c % 2 == 1)
-					printf(" ");
-			}
-			for (; i < size; i++)
-			{
-				if (b[i] < 32)
-					ptintf(".");
-				else
-					printf("%c", b[i]);
-			}
-			printf("\n");
-		}
-	}
-/**
 	while (c < size)
 	{
-		printf("%8.8x", c);
+		printf("%0.8x", c);
 		d = 0;
 
 		while (d < 10)
 		{
-			printf("%02x", b[c + d]);
+			printf("%0.2x", b[c + d]);
 			if ((d % 2 == 0 && d != 0) || (c + d > size - 1))
 		 	{
 				printf(" ");
@@ -77,5 +28,4 @@ void print_buffer(char *b, int size)
 		c += 10;
 		printf("\n"); 
 	}
-*/
 }
