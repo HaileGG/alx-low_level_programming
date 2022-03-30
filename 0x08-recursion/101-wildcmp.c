15ff29a265dc6f5b710befd6aa322a299c559fce
Haile Gebresilassie <hailye2013@gmail.com>
2022-03-29 23:49:32 -0700

    Completed task 8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -23,7 +23,7 @@ int wildcmp(char *s1, char *s2)
 		{
 			return (0);
 		}
-		if (wildcmp(s1, s2 + 1) !! wildcmp(s1 + 1, s2))
+		if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
 		{
 			return (1);
 		}
