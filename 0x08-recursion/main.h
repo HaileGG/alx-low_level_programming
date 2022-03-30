e1cca52373fd4f3bb6a129084fec710225d809b1
Haile Gebresilassie <hailye2013@gmail.com>
2022-03-29 21:43:17 -0700

    commit

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,14 @@
+#ifndef _main_h_
+#define _main_h_
+
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int _putchar(char c);
+
+#endif
