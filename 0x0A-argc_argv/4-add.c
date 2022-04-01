5302b844f401d4cbd5e102526d57c00a888bcd4e
Haile Gebresilassie <hailye2013@gmail.com>
2022-03-31 22:40:17 -0700

    Completed task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * main - Prints the integer arguments and the sum.
+ * @argc: Number of command line arguments.
+ * @argv: Array name.
+ * Return: 1
+ */
+int main(int argc, char *argv[])
+{
+	int i, j, len, sum;
+	char *ptr;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		sum = 0;
+		for (i = 1; i < argc; i++)
+		{
+			ptr = argv[i];
+			len = strlen[ptr];
+
+			for (j = 0; j < len; j++)
+			{
+				if (isdigit(*(ptr + j)) == 0)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+
+			sum += atoi(argv[i]);
+		}
+		printf("%d\n", sum);
+	}
+	return (0);
+}
