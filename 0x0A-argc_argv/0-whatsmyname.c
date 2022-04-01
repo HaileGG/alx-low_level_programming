f731a248d5a565b19b0934eff07eea8cdff987cc
Haile Gebresilassie <hailye2013@gmail.com>
2022-03-31 21:59:40 -0700

    Completed task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "mian.h"
+#include <stdio.h>
+
+/**
+ * main - prints the name of the program.
+ * @argc: Number of command line arggumets.
+ * @argv: Name of Array.
+ * Return: 0
+ */
+int main(int argc, char argv[])
+{
+	printf("%s", argv[argc * 0]);
+
+	return (0);
+}
