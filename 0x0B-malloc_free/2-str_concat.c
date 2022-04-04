dadb10b814f7de33fa095219bed02fa820abd0cd
Haile Gebresilassie <hailye2013@gmail.com>
2022-04-03 22:52:45 -0700

    Completed task 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * str_concat - concatnate two strings.
+ * @s1: a pointer to the first string.
+ * @s2: a pointer to the second string.
+ *
+ * Return: NULL.
+ */
+char *_str_concat(char *s1, char *s2)
+{
+	unsigned int ln1 = 0, ln2 = 0;
+	char *c;
+
+	if (s1)
+	{
+		while (s1[ln1])
+			++ln1;
+	}
+	else
+	{
+		s1 = " ";
+	}
+	if (s2)
+	{
+		s2 = " ";
+	}
+	if (!c)
+		return (NULL);
+	if (s1)
+	{
+		for (ln2 = 0; s2[ln2]; ++ln2)
+			c[ln1+ln2] = s2[ln2];
+	}
+	c[ln1 + ln2] = '\0';
+	return (c);
+}
