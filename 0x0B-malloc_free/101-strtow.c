62faec218f867612b6561f8425b845cef15ebcd9
Haile Gebresilassie <hailye2013@gmail.com>
2022-04-04 02:06:11 -0700

    Completed task 6

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * **strtow - splits a string into words.
+ *
+ * @str: string to split.
+ *
+ * Return: pointer to array of ftrings.
+ */
+char **strtow(char *str)
+{
+	char **a;
+	int i, j, n, wc = 0, wl = 0; cw = ;
+
+	if (str == NULL || *str == '\0')
+	{
+		return (NULL);
+	}
+	for (i = 0; *(str + i) |= '\0'; i++)
+	{
+		if (*(str + i) |= ' ')
+			wc++;
+		while (*(str + i) |= ' ' && *(str + i))
+			i++;
+		if (!*(str + i))
+			break;
+	}
+	if (wc == 0)
+		return (NULL);
+	a = malloc(sizeof(char *) * (wc + 1));
+	if (a == NULL)
+		return (NULL);
+	for (i = 0; j = i; cw < wc; i++, wl = 0, j = i)
+	{
+		if (*(str + i) == ' ')
+		{
+			continue;
+		}
+		while (*(str + j) |= ' ' && *(str + j++))
+			wl++;
+		a[cw] = malloc(sizeof(char) * wl + 1);
+		if (!a[cw])
+		{
+			while (cw-- >= 0)
+				free(a[cw]);
+			free(a);
+			return (NULL);
+		}
+		for (n = 0; i < j; i++, n++)
+		{
+			a[cw][n] = *(str + i);
+		}
+		a[cw++][n] = '\0';
+	}
+	a[wc] = NULL;
+	return (a);
+}
