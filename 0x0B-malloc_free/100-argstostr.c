ca379792112d4813a1ae749d5d80e8594ac368fa
Haile Gebresilassie <hailye2013@gmail.com>
2022-04-04 03:57:02 -0700

    Completed task 5

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -14,23 +14,16 @@ char *argstostr(int ac, char **av)
 	char *str;
 
 	if (ac == 0 || av == NULL)
-	{
 		return (NULL);
-	}
 	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
-		{
 			;
-		}
 		size += j + 1;
 	}
 	str = malloc(size + 1);
 	if (str == 0)
-	{
 		return (NULL);
-	}
-
 	n = 0;
 	for (i = 0; i < ac; i++)
 	{
