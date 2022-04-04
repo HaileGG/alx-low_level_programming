64507beebe846b3514fb638305643f2ab4ceb2b8
Haile Gebresilassie <hailye2013@gmail.com>
2022-04-04 01:27:33 -0700

    Completed task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -36,13 +36,13 @@ char *str_concat(char *s1, char *s2)
 	{
 		return (NULL);
 	}
-	for (j = 0; *(s1 + i) != '\0'; i++)
+	for (i = 0; *(s1 + i) != '\0'; i++)
 	{
 		c[i] = *(s1 + i);
 	}
 	for (j = 0; *(s2 + j) != '\0'; j++)
 	{
-		c[j] = *(s2 + j);
+		c[i + j] = *(s2 + j);
 	}
 	c[i + j] = *(s2 + j);
 	return (c);
