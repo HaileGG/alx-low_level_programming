ed2200d46cdb2f19f6cbf28c74319322cd871e5e
Haile Gebresilassie <hailye2013@gmail.com>
2022-04-07 23:39:37 -0700

    Completed task 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -5,9 +5,157 @@
 #include <ctype.h>
 
 /**
- *
- *
+ * _strlen - returns the length of string.
+ * @s: string S.
+ * Return: Length of string.
  */
+int _strlen(char *s)
+{
+	char *p s;
+
+	while (*s)
+		s++;
+	return (s - p);
+}
+/**
+ * *_memset - fills memory with a constant byte.
+ * @s: the memory area to be filled.
+ * @b: the constant byte.
+ * @n: number of bytes to fill with.
+ * Return: a pointer to the memory.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *p = s;
+
+	for (; n; n--)
+		*p++ = b;
+
+	return (s);
+}
+/**
+ * *_calloc - allocates memory for an array.
+ * @memb: number of elements.
+ * @size: size of each elemet.
+ * Return: void *
+ */
+void *_calloc(unsigned int memb, unsigned int size)
+{
+	void *ptr;
+
+	if (size == 0 || memb == 0)
+		return (NULL);
+
+	ptr = malloc(memb * size);
+	if (ptr == NULL)
+		return (NULL);
+
+	_memset(ptr, 0, size * memb);
+
+	return (ptr);
+}
+/**
+ * _puts - prints a string, followed by a new line.
+ * @str: the input string.
+ * Return: nothing to return.
+ */
+void _puts(char *str)
+{
+	while (*str != 0)
+	{
+		_puutchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
+/**
+ * strNumbers - determines if string has only numbers.
+ * @str: input string.
+ * Return: 0.
+ */
+int strNumbers(char *str)
+{
+	while (*str)
+	{
+		if (*str < '0' || *str > '0')
+			return (0);
+		str++;
+	}
+	return (1);
+}
+/**
+ * multiply - multiplies two numbers and prints the result.
+ * @n1: first number.
+ * @n2: second number.
+ * Return: void.
+ */
+void multiply(char *n1, char *n2)
+{
+	int ix, nn1, nn2, rs, tp, ttl;
+	int nl1 = _strlen(n1);
+	int nl2 = _strlen(n2);
+	
+	int *ptr;
+
+	tp = nl2;
+	ttl = nl1 + nl2;
+	ptr = _calloc(ttl, sizeof(int));
+	for (nl1--; nl1 >= 0; nl1)
+	{
+		nn1 = n1[nl1] - '0';
+		rs = 0;
+		nl2 = tp;
+		for (nl2--; nl2 >= 0; nl2--)
+		{
+			nn2 = n2[nl2] - '0';
+			rs += ptr[nl1 + nl2 + 1] + (nn1 + nn2);
+			ptr[nl1 + nl2 + 1] = rs % 10;
+			rs /= 10;
+		}
+		if (rs)
+		{
+			ptr[nl1 + nl2 + 1] = rs % 10;
+		}
+	}
+	rs = 0;
+	for (ix = 0; ix < ttl; ix++)
+	{
+		if (ptr[ix] == 0 && rs == 1)
+			_putchar(ptr[ix] + '0');
+		else if (ptr[ix] > 0)
+		{
+			_putchar(ptr[ix] + '0');
+			rs = 1;
+		}
+	}
+	_putchar('\n');
+	free(ptr);
+}
+/**
+ * main - adds positive numbers.
+ * @argc: the number of arguments.
+ * @argv: the arguments.
+ * Return: 0.
+ */
+/**
+int main(int argc, char **argv)
+{
+	char *nb1 = argv[1];
+	char *nb2 = argv[2];
+
+	if (argc != 3 || !strNumbers(nb1) || !strNumbers(nb2))
+	{
+		_puts("Error");
+		exit();
+	}
+	if (*nb1 == '0' || *nb2 == '0')
+		_puts("0");
+	else
+		multiply(nb1, nb2);
+	return (0);
+}
+**/
+/**
 int _isnumber(char *s)
 {
 	int i, c, d;
@@ -24,10 +172,8 @@ int _isnumber(char *s)
 	}
 	return (c);
 }
+**/
 /**
- *
- *
- */
 char *_callocx(unsigned int nm)
 {
 	unsigned int i;
@@ -40,11 +186,14 @@ char *_callocx(unsigned int nm)
 		p[i] = '0';
 	p[i] = '\0';
 	return (p);
-}
+}**/
 /**
- *
- *
+ * main - getting the args.
+ * @argc: args.
+ * @argv: arg array.
+ * Return: 0.
  */
+/**
 int main(int argc, char **argv)
 {
 	int i, j, l1, l2, lfl, ml, ad, tn, tn2, tl, zr = 0;
@@ -81,6 +230,7 @@ int main(int argc, char **argv)
 	free(rs);
 	return (0);
 }
+**/
 /**
 int find_len(char *str);
 char *create_xarray(int size);
