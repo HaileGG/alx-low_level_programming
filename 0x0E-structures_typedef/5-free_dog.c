a8e450fa46f536a5ecec4b0aadd6a66c4e91bfcf
Haile Gebresilassie <hailye2013@gmail.com>
2022-04-11 05:08:20 -0700

    Completed task 5

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
 
 /**
  * free_dog - initializes a variable of type struct dog.
  * @d: dog's details.
+ *
+ * Return: 0
  */
 
 void free_dog(dog_t *d)
 {
-	if (d)
+	if (d != NULL)
 	{
 		free(d->name);
 		free(d->owner);
