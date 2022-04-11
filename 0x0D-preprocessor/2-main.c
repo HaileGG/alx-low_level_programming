9c4708f5348665a9d8705aec51030255d74355be
Haile Gebresilassie <hailye2013@gmail.com>
2022-04-11 03:55:28 -0700

    Completed task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#ifndef _MAIN_
+
+/**
+ * main - Prints the file name of the program.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("%s\n", _FILE_);
+	return (0);
+}
