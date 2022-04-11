62003a40042a70ec2f00ef511df1489d14845fe2
Haile Gebresilassie <hailye2013@gmail.com>
2022-04-11 04:26:56 -0700

    Completed task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,20 +1,25 @@
 #ifndef _DOG_H_
 #define _dog_h_
 
+typedef struct dog dog_t;
 /**
  * struct dog - structure.
  * @name: dog's name.
  * @age: dog's age.
  * @owner: dog's owner
+ *
+ * Desc: longer description.
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 }
-dog_t;
-void init_dog(struct dog *d, char name *anme, float age, char *owner);
+
+int _putchar(char c);
+
+void init_dog(struct dog *d, char *anme, float age, char *owner);
 
 void pring_dog(struct dog *d);
 
