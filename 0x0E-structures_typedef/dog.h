42f9eac019605f940e8227293b815866eab8cf63
Haile Gebresilassie <hailye2013@gmail.com>
2022-04-11 05:22:20 -0700

    Completed task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,9 +18,8 @@ struct dog
 	char *owner;
 };
 
-int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
-void pring_dog(struct dog *d);
+void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
