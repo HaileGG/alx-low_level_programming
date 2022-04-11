aab1d0b51a0bd5adaef38cde9a6157286b702f4d
HaileGG <99530961+HaileGG@users.noreply.github.com>
2022-04-11 16:24:53 +0300

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -8,7 +8,7 @@
  * @owner: owner of dog.
  * Return: new dog.
  */
-dog_t *new_dog(char *name, float age, *char *owner)
+dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *nd;
 	int n1, n2, i;
