c565edacb62b4a0d5f221003a2c02124bffa4265
Haile Gebresilassie <hailye2013@gmail.com>
2022-04-14 04:32:00 -0700

    Completed task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -24,7 +24,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(nums, int));
-		if (n == i - 1)
+		if (n == i + 1)
 		{
 			break;
 		}
