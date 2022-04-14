44120d9d93a1d75447ab344619661d13a36f788e
Haile Gebresilassie <hailye2013@gmail.com>
2022-04-14 05:49:19 -0700

    Completed task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,26 +12,33 @@
  */
 int main(int argc, char *argv[])
 {
-	int n1, n2, re;
+	int n1, n2, res;
 	int (*oprt)(int, int);
 	
-	if (argc != 4)
+	if (argc == 4)
 	{
-		printf("Error\n");
-		exit(98);
+		if (argv[2][1] != '\0')
+		{
+			printf("Error\n");
+			exit(98);
+		}
+		oprt = get_op_func(argv[2]);
+		if (oprt == NULL)
+		{
+			printf("Error\n");
+			exit(99);
+		}
+
+		n1 = atoi(argv[1]);
+		n2 = atoi(argv[3]);
+		res = oprt(n1, n2);
+
+		printf("%d\n", res);
+		return (0);
 	}
-
-	n1 = atoi(argv[1]);
-	n2 = atoi(argv[3]);
-	oprt = get_op_func(argv[2]);
-
-	if (oprt == NULL)
+	else
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	re = oprt(n1, n2);
-
-	printf("%d\n", re);
-	return (0);
 }
