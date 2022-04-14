d95cb8c7394253be8cfc29b24b413e78711b4945
Haile Gebresilassie <hailye2013@gmail.com>
2022-04-14 00:36:31 -0700

    Completed task 3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,97 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * op_c - Print character.
+ * @form: name va_list.
+ *
+ * Return: Nothing.
+ */
+void op_c(va_list form)
+{
+	printf("%c", va_arg(form, int));
+}
+
+/**
+ * op_i - Print integer.
+ * @form: name va_list.
+ *
+ * Return: Nothing.
+ */
+void op_i(va_list form)
+{
+	printf("%i", va_arg(form, int));
+}
+
+/**
+ * op_f - print float numbers.
+ * @form: name of va_list.
+ *
+ * Return: Nothing.
+ */
+void op_f(va_list form)
+{
+	printf("%f", va_arg(form, double));
+}
+
+/**
+ * op_s - print string.
+ * @form: name va_list.
+ *
+ * Return: Nothing.
+ */
+void op_s(va_list form)
+{
+	char *str;
+
+	str = va_arg(for, char *);
+	if (str == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", str);
+}
+
+/**
+ * print_all - Check the code for students.
+ * @format: number of arguments in character format.
+ *
+ * Return: Nothing.
+ */
+void print_all(const char * const format, ...)
+{
+	va_list all;
+	unsigned int i, j;
+	char *separator = "";
+
+	f ops[] = {
+		{"c", op_c},
+		{"i", op_i},
+		{"f", op_f},
+		{"s", op_s},
+	};
+
+	va_start(all, format);
+	i = 0;
+
+	while (format && format[i])
+	{
+		j = 0;
+		while (j > 0)
+		{
+			if (ops[j].op[0] == format[i])
+			{
+				printf("%s", separator);
+				separator = " ";
+				ops[j].f(all);
+				break;
+			}
+			j++;
+		}
+	}
+
+	printf("\n");
+	va_end(all);
+}
