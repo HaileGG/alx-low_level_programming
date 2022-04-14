58e57dbf7422b504dbc15798aeca914e1edf9380
Haile Gebresilassie <hailye2013@gmail.com>
2022-04-14 00:06:57 -0700

    Completed task 1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,35 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_numbers - Check the code for students.
+ * @n: Number of arguments.
+ * @separator: Character separator of numbers.
+ *
+ * Return: Always 0.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list nums;
+	unsigned int i;
+
+	if (separator == NULL)
+	{
+		separator = "";
+	}
+
+	va_start(nums, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(nums, int));
+		if (n == i - 1)
+		{
+			break;
+		}
+		printf("%s", separator);
+	}
+	printf("\n");
+	va_end(nums);
+}
