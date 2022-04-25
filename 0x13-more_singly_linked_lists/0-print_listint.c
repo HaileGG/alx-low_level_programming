49eff67874e3dffa92342900bbb3526224acc6a8
Haile Gebresilassie <hailye2013@gmail.com>
2022-04-25 20:10:49 +0300

    My Completed task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -16,11 +16,14 @@ size_t print_listint(const listint_t *h)
 {
 	size_t nodes = 0;
 
+	if (!h)
+		return (0);
+
 	while (h)
 	{
+		nodes += 1;
 		printf("%d\n", h->n);
 		h = h->next;
-		nodes++;
 	}
 
 	return (nodes);
