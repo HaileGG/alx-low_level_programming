25bfbc194412f430b1e4efbfc5e093d9f93f3d09
Haile Gebresilassie <hailye2013@gmail.com>
2022-04-27 22:50:26 -0700

    My main header commit

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,18 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+ * File: main.h
+ * Auth: Haile G
+ * Desc: Header file
+ */
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endiamess(void);
+
+#endif /** _MAIN_H_ */
