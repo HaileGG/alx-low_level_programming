cf4ab6419505474122e65496107b46c4c855d244
Haile Gebresilassie <hailye2013@gmail.com>
2022-04-28 04:46:09 -0700

    My Completed task 6

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * get_endianness - Checks the endianness.
@@ -11,6 +12,8 @@ int get_endianness(void)
 	char *endn =(char *)&num;
 
 	if (*endn == 1)
+	{
 		return (1);
+	}
 	return (0);
 }
