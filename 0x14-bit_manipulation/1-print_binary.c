65e5f580b99dd0a632959bd7e53468acbd9fc3ea
Haile Gebresilassie <hailye2013@gmail.com>
2022-04-28 04:57:43 -0700

    My Completed task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,8 +1,3 @@
-/**
- * File: 1-print_binary.c
- * Auth: Haile G
- */
-
 #include "main.h"
 
 /**
@@ -13,6 +8,5 @@ void print_binary(unsigned long int n)
 {
 	if (n > 1)
 		print_binary(n >> 1);
-
 	_putchar((n & 1) + '0');
 }
