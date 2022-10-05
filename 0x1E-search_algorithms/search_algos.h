551b81c9d6c882910e36043eb181e40fb1967288
Haile Gebresilassie <hailye2013@gmail.com>
2022-10-05 21:45:28 +0300

    My Header Task

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,15 +1,14 @@
-#ifndef _BINARY_SEARCH_
-#define _BINARY_SEARCH_
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-
+int binary_recur(int *array, size_t start, size_t end, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
-int exponential_search(int *array, size_t size, int value);
-int advanced_binary(int *array, size_t size, int value);
+int interpolation_recur(int *array, size_t start, size_t end, int value);
 
-#endif
+#endif /* SEARCH_ALGOS_H */
